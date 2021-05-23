aac4f9de161c6071cfc4c5f501696aa4a77dab18
naisthorpe <aisthorpe@gmail.com>
2021-05-23 12:06:19 -0400

    Fix useFindAndModify value in server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: true
+    useFindAndModify: false
   });
 
 /* HTML Routes */
